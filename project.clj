51ed0da98f5395b3b69a75810a66dbcb23d4d626
Wojtek Majewski <jumski@gmail.com>
2020-02-29 14:45:37 +0100

    Read and group events from path

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject midi-dataset-toolkit "0.1.0-SNAPSHOT"
+  :description "Toolkit for reading midi files and processing them to create a dataset of musical steps"
+  :url "https://github.com/jumski/midi-dataset-toolkit"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [overtone "0.10.6"]]
+  :repl-options {:init-ns midi-dataset-toolkit.core})
---
+++ src/midi_dataset_toolkit/core.clj
@@ -0,0 +1,32 @@
+(ns midi-dataset-toolkit.core
+  (:require [overtone.midi.file :as midifile]))
+
+(defn note-on?
+  "Returns true if event is a note on"
+  [event]
+  (and (= :note-on (:command event))
+       (not (nil? (:note event)))))
+
+(defn read-note-ons-from-file
+  "Returns a list of events, flattened from all tracks in midi path from path"
+  [path]
+  (->> (midifile/midi-file path)
+      (:tracks)
+      (map :events)
+      (flatten)
+      (filter note-on?)))
+
+(defn group-by-timestamp
+  "Returns map of timestamp to list of events at given timestamp"
+  [events]
+  (group-by :timestamp events))
+
+(->> (read-note-ons-from-file "resources/c_major_scale.mid")
+    (group-by-timestamp)
+    (sort #(< (first %1) (first %2)))
+    (map count))
+
+; (->> (all-ns)
+;      (map ns-name)
+;      (map name)
+;      (filter #(clojure.string/includes? % "overtone")))
