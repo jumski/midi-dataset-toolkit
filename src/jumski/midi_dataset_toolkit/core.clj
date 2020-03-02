9d2bf57ca1f8c0dce418f939973ae88bfd030a00
Wojtek Majewski <jumski@gmail.com>
2020-03-02 16:50:50 +0100

    Add documentation

--- src/jumski/midi_dataset_toolkit/core.clj
+++ src/jumski/midi_dataset_toolkit/core.clj
@@ -2,9 +2,11 @@
   (:require [jumski.midi-dataset-toolkit.toolkit :as toolkit])
   (:gen-class))
 
-(defn -main [& args]
+(defn -main
+  "Runs `midi-file-to-steps-string` for each `arg` of `args` and prints to stdout"
+  [& args]
   (if (empty? args)
-    (println "Please provide path to midi file!")
+    (println "Please provide path to midi file or files!")
     (doseq [path args
             :let [steps-string (toolkit/midi-file-to-steps-string path)]]
       (println steps-string))))
